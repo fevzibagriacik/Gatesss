b3b37f194e4c3a63d5808d7536051139ac7de777
Fevzi Bağrıaçık <fevzi.bagriacik1905@gmail.com>
2024-12-14 11:43:31 +0300

    HealthBar is created

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float speed = 5f;
+    [SerializeField] float healthAmount = 100f;
+    [SerializeField] float takeDamage;
+
+    [SerializeField] bool isDead = false;
+
+    [SerializeField] Image healthBar;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isDead)
+        {
+            HorizontalMove();
+        }
+        else
+        {
+            Respawn();
+        }
+    }
+
+    void HorizontalMove()
+    {
+        Vector2 dir = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+        }
+
+        dir.Normalize();
+
+        GetComponent<Rigidbody2D>().velocity = speed * dir;
+    }
+
+    void TakeDamage(float damage)
+    {
+        healthAmount -= damage;
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    void fillHealth()
+    {
+        healthBar.fillAmount = 100f;
+        healthAmount = 100f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Barrier"))
+        {
+            TakeDamage(15);
+
+            if(healthBar.fillAmount <= 0f)
+            {
+                isDead = true;
+            }
+        }
+    }
+
+    void Respawn()
+    {
+        isDead = false;
+
+        transform.position = new Vector3(-0.75f, 1.12f, 0f);
+
+        healthAmount = 100f;
+        healthBar.fillAmount = 100f;
     }
 }
