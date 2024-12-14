7d7000f95072293f8bf55c4798a5d5c970d8ea23
Fevzi Bağrıaçık <fevzi.bagriacik1905@gmail.com>
2024-12-14 16:17:53 +0300

    SlideWall is created

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,37 +8,73 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float healthAmount = 100f;
     [SerializeField] private float takeDamage;
+    [SerializeField] private float jumpForce = 16f;
+    [SerializeField] private float wallSlidingSpeed = 2f;
+    [SerializeField] private float wallJumpingTime = 0.2f;
+    [SerializeField] private float wallJumpingCounter;
+    [SerializeField] private float wallJumpingDuration = 0.4f;
 
     [SerializeField] private bool isDead = false;
-    [SerializeField] private bool isWallTouch = false;
+    [SerializeField] private bool isGround;
+    [SerializeField] private bool isWallSliding;
+    [SerializeField] private bool isMoving = false;
+    [SerializeField] private bool isWallJumping = false;
 
     [SerializeField] private Image healthBar;
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
+
+    [SerializeField] private Vector2 wallJumpingPower = new Vector2(8f, 16f);
 
     void Update()
     {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
         if (!isDead)
         {
-            HorizontalMove();
+            //HorizontalMove();
         }
-        else
+        else if(isDead)
         {
             Respawn();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isDead && isGround)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        WallSlide();
+
+        WallJump();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isWallJumping && !isDead)
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
         }
     }
 
     void HorizontalMove()
     {
+        isMoving = false;
         Vector2 dir = Vector2.zero;
         if (Input.GetKey(KeyCode.A))
         {
+            isMoving = true;
             dir.x = -1;
         }
         else if (Input.GetKey(KeyCode.D))
         {
+            isMoving = true;
             dir.x = 1;
         }
 
@@ -80,5 +116,52 @@ public class PlayerController : MonoBehaviour
 
         healthAmount = 100f;
         healthBar.fillAmount = 100f;
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    void WallSlide()
+    {
+        if (IsWalled() && !isGround && isMoving)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+        }
+
+        Invoke(nameof(StopWallJumping), wallJumpingDuration);
+    }
+
+    void StopWallJumping()
+    {
+        isWallJumping = false;
     }
 }
